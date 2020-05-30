ffbbfc270e41cecc882f4f2cb23fef238c5bdc0e
ksby <kusa@db3.so-net.ne.jp>
2020-05-30 06:18:51 +0000

    #3 Lambda に翻訳結果を translate-history テーブルに登録する処理を追加しました

--- aws-handson-for-beginners-serverless-1/translate-function/handler.py
+++ aws-handson-for-beginners-serverless-1/translate-function/handler.py
@@ -1,15 +1,16 @@
+import datetime
 import json
 import logging
+
 import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 translate = boto3.client('translate')
-
+dynamodb_translate_history_tbl = boto3.resource('dynamodb').Table('translate-history')
 
 def lambda_handler(event, context):
-
     # logger.info(event)
 
     input_text = event['queryStringParameters']['input_text']
@@ -21,6 +22,14 @@ def lambda_handler(event, context):
     )
 
     output_text = response.get('TranslatedText')
+
+    dynamodb_translate_history_tbl.put_item(
+        Item={
+            'timestamp': datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
+            'input_text': input_text,
+            'output_text': output_text
+        }
+    )
 
     return {
         'statusCode': 200,
